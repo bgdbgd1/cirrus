ff5253d77f0b3c7e0fe73346819d891fbb0d2736
Bogdan Ene <b.ene@nelson.nl>
2021-09-19 18:43:00 +0200

    update raise err 2

--- src/CriteoCsvToLibSVM.cpp
+++ src/CriteoCsvToLibSVM.cpp
@@ -70,7 +70,7 @@ int main() {
      fclose(file);
      std::cout << "file exists" << std::endl;
   } else {
-     std::runtime_error << "file DOES NOT exist!!";
+    throw std::runtime_error("file DOES NOT exist!!");
   }
   config.check();
 
